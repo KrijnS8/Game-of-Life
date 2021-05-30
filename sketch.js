b5e1cbcadcd46e8986ec087e39bb66c146270570
KrijnS8 <krijn.schelvis@gmail.com>
2021-05-30 22:45:08 +0200

    .

--- sketch.js
+++ sketch.js
@@ -1,20 +1,67 @@
 let size = 20;
+let cellState = [];
 
-let cells = [];
+let updateInterval;
+let updateSpeed = 500;
+let mouseInterval;
+let mouseMemory = [];
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
 
-    for(let i = 0; i < window.innerWidth / size; i++) {
-        cells.push([]);
+    for(let x = 0; x < window.innerWidth / size; x++) {
+        cellState.push([]);
+        for(let y = 0; y < window.innerHeight / size; y++) {
+            cellState[x][y] = false;
+        }
     }
 }
 
 function draw() {
-  for(let y = 0; y < window.innerHeight / size; y++) {
-      for(let x = 0; x < window.innerWidth / size; x++) {
-          rect(x * size, y * size, size, size);
+    push();
+    stroke(150);
+    for(let x = 0; x < cellState.length; x++) {
+        for(let y = 0; y < cellState[x].length; y++) {
+            if(cellState[x][y] === false) {fill('white');} else {fill('black');}
+            rect(x * size, y * size, size, size);
+        }
+    }
+    pop();
+}
 
-      }
-  }
+function mousePressed() {
+    mouseInterval = setInterval(() => {
+        let x = Math.floor(mouseX / size);
+        let y = Math.floor(mouseY / size);
+
+        if(mouseMemory.includes(`${x}${y}`)) {
+            return
+        }
+        cellState[x][y] = !cellState[x][y];
+        mouseMemory.push(`${x}${y}`);
+    }, 10);
+}
+
+function mouseReleased() {
+    clearInterval(mouseInterval);
+    mouseMemory = [];
+}
+
+function keyPressed() {
+    if(keyCode === 32 && updateInterval === undefined) {
+        updateInterval = setInterval(update, updateSpeed);
+    } else if(keyCode === 32 && updateInterval !== undefined) {
+        clearInterval(updateInterval);
+        updateInterval = undefined;
+    }
+
+    let newCellState = cellState;
+
+    function update() {
+        for(let x = 0; x < cellState.length; x++) {
+            for(let y = 0; y < cellState[x].length; y++) {
+
+            }
+        }
+    }
 }
